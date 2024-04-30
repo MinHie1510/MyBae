95d991e29783d2ec6fc56fffaa953549f3b993ec
khaihien <0306211452@caothang.edu.vn>
2024-04-30 07:03:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function fadeIn(){
+    var text = $('.content').text().replace(/^\s+|\s+$/g,'');
+    var length = text.length;
+    var i = 0;
+    var txt;
+    var html = [];
+    var sp = 4;
+    for(;i<length;i+=sp) {
+        txt = text.substring(i,i+sp);
+        html.push('<span class="c animated">' + txt + '</span>');
+    }
+    $('.content').removeClass('c').html(html.join(''));
+
+    for(i = 0,length = html.length;i<length;i++) {
+        !function(i){
+            setTimeout(function(){
+                $('.content').find('.animated').eq(i).addClass('fadeIn');
+            },i*400);
+        }(i);
+    }
+};
+
+
+
+// document.querySelector(".content").onclick=()=>{
+//     document.querySelector("#heart").hidden=false
+//     document.querySelector("body").style.backgroundColor="#ffffff"
+//     document.querySelector("#heart").hidden=false
+//     fadeIn()
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Code sẽ tự động chạy khi trang được tải hoàn toàn
+//     document.querySelector("#heart").hidden = false;
+//     document.querySelector("body").style.backgroundColor = "#ffffff";
+//     document.querySelector("#heart").hidden = false;
+//     fadeIn();
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Code sẽ tự động chạy khi trang được tải hoàn toàn
+
+    // Bắt đầu phát âm thanh
+    // var audio = document.getElementById("myAudio");
+    // audio.play();
+
+    setTimeout(function() {
+        document.querySelector("#heart").hidden = false;
+        document.querySelector("body").style.backgroundColor = "#ffffff";
+        document.querySelector("#heart").hidden = false;
+        fadeIn();
+    }, 3000); // 3000 milliseconds = 3 giây
+});
